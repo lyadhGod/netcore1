f19db209b97b327aae26d892bb2c6270069f47f1
Rounak Tikadar <rounak.tikadar@gmail.com>
2022-02-12 13:13:33 +0530

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import {ColorEvent} from 'ngx-color';
+
+import carsData from '../data/cars';
+import partsData from '../data/parts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor () { }
+
+  @ViewChild('partInput') partInput!: ElementRef<HTMLInputElement>;
+
   title = 'netcore1';
+
+  carMaxLength = 75;
+  carMaxLengthText = `Max. ${this.carMaxLength} characters allowed`;
+  car = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(this.carMaxLength),
+    Validators.pattern(/^[a-zA-Z]+$/),
+  ]);
+  carValidationMessage () {
+    if (this.car.errors?.['required']) return 'Full name is required';
+    if (this.car.errors?.['maxLength']) return this.carMaxLengthText;
+    if (this.car.errors?.['pattern']) return 'Only english alphabets allowed';
+    return '';
+  }
+
+  carsData = carsData;
+  getFilteredCarsData () {
+    return this.carsData.filter(({brand}) => brand.match(new RegExp(`^${this.car.value}`, 'i')))
+  }
+
+  part = new FormControl('');
+
+  partsData = partsData;
+  getFilteredPartsData () {
+    return this.partsData.filter((part) => part['List of auto parts'].match(new RegExp(`^${this.part.value}`, 'i')))
+  }
+
+  selectedParts: typeof partsData = [];
+  addSelectedPart ($event: MatAutocompleteSelectedEvent) {
+    const partIndex = this.partsData
+    .findIndex((part) => part['List of auto parts'].localeCompare($event.option.value) === 0)
+
+    this.selectedParts.push(this.partsData[partIndex]);
+    this.selectedParts = [...this.selectedParts];
+
+    this.partsData.splice(partIndex, 1);
+    this.partsData = [...this.partsData];
+
+    this.partInput.nativeElement.value = '';
+    this.part.setValue('')
+  }
+  removeSelectedPart (index: number) {
+    this.partsData.push(this.selectedParts[index]);
+    this.partsData = [...this.partsData];
+
+    this.selectedParts.splice(index, 1);
+    this.selectedParts = [...this.selectedParts];
+  }
+
+  colorMaxLength = 6;
+  colorMaxLengthText = `Max. ${this.colorMaxLength} characters allowed`;
+  colorHexText = 'Color in HEX format required';
+  color = new FormControl('c4c4c4', [
+    Validators.required,
+    Validators.maxLength(this.colorMaxLength),
+    Validators.pattern(/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/),
+
+  ]);
+  colorValidationMessage () {
+    if (this.color.errors?.['required']) return 'Color required';
+    if (this.color.errors?.['maxLength']) return this.colorMaxLengthText;
+    if (this.color.errors?.['pattern']) return this.colorHexText;
+    return '';
+  }
+
+  colorChange ($event: ColorEvent) {
+    this.color.setValue($event.color.hex.substring(1));
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { ColorSketchModule } from 'ngx-color/sketch';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +21,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatCardModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatIconModule,
+    ColorSketchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
